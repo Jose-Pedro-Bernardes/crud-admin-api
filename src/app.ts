fc9c4eb0cee31ee1e64d54d9368718a327b3626e
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-04-28 16:27:44 -0300

    feat: criando duas rotas e passando as sub-rotas.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
-import express, { Application, json } from 'express'
+import express, { Application, json } from "express";
+import { usersRouter } from "./routers/users.router";
+import { loginRouter } from "./routers/login.router";
 
-const app: Application = express()
-app.use(json())
+const app: Application = express();
+app.use(json());
 
-export default app
+app.use("/users", usersRouter);
+app.use("/login", loginRouter);
+
+export default app;
