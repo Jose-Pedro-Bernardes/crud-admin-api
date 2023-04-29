d7e423f6cc9a8869435f3bf625f2db17039e2d90
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-04-29 20:16:13 -0300

    feat: organizando projeto.

---
+++ src/services/users/createUser.services.ts
@@ -0,0 +1,33 @@
+import { TUserRequest } from "../../interfaces/users.interfaces";
+import { hashSync } from "bcryptjs";
+import format from "pg-format";
+import { client } from "../../database";
+import { usersInfoSchema } from "../../schemas/users.schemas";
+import { QueryResult } from "pg";
+import { TUserCreate } from "../../interfaces/users.interfaces";
+
+const createUserService = async (
+  payload: TUserRequest
+): Promise<TUserCreate> => {
+  payload.password = hashSync(payload.password, 10);
+  if (!payload.active) {
+    payload.active = false;
+  }
+
+  const keys = Object.keys(payload);
+  const values = Object.values(payload);
+  const queryString: string = format(
+    `
+  INSERT INTO users (%I) VALUES (%L) RETURNING *;
+  `,
+    keys,
+    values
+  );
+
+  const response: QueryResult<TUserRequest> = await client.query(queryString);
+  const user = usersInfoSchema.parse(response.rows[0]);
+
+  return user;
+};
+
+export default createUserService;
