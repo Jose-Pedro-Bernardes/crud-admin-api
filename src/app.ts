fd4c0435307a37f105168646f5dbc2a16e1c1a22
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-04-29 20:00:58 -0300

    feat: definindo rotas principais da aplicação e adicionando o errorHandler responsável por verificar os erros.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express, { Application, json } from "express";
-import { usersRouter } from "./routers/users.router";
-import { loginRouter } from "./routers/login.router";
+import { usersRouter } from "./routes/users.routes";
+import { loginRouter } from "./routes/login.routes";
+import { errorHandler } from "./error";
 
 const app: Application = express();
 app.use(json());
 
 app.use("/users", usersRouter);
 app.use("/login", loginRouter);
+
+app.use(errorHandler);
 
 export default app;
