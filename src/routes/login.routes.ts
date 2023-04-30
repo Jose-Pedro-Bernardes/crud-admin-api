96d062f144f71d7544534a1c4249f7bf7e6a76e0
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-04-30 01:26:56 -0300

    feat: adiciona verificação e lógica para a rota de login de usuario.

--- src/routes/login.routes.ts
+++ src/routes/login.routes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import verifyingPayload from "../middlewares/ensureBodyIsValid.middlewares";
+import { usersLoginSchema } from "../schemas/schemas";
+import loginUser from "../controllers/login.controller";
 
 const loginRouter = Router();
 
-loginRouter.post("");
+loginRouter.post("", verifyingPayload(usersLoginSchema), loginUser);
 
 export { loginRouter };
