24fc6d9b7a0757dc4dc52c09df7967c780f34126
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-04-30 00:21:37 -0300

    feat: Adiciona controller para gerar e enviar token de autenticação.

--- src/controllers/login.controller.ts
+++ src/controllers/login.controller.ts
@@ -1,5 +1,12 @@
 import { Request, Response } from "express";
+import userLoginService from "../services/session/userLogin.services";
+import { TLoginPayload } from "../interfaces/login.interfaces";
 
 const loginUser = async (req: Request, res: Response): Promise<Response> => {
-  return res.status(201).json();
+  const payload: TLoginPayload = req.body;
+  const token: string = await userLoginService(payload);
+
+  return res.status(200).json({ token: token });
 };
+
+export default loginUser;
