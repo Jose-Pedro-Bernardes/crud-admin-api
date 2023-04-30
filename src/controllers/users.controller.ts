0a21835e0aaa1c82bd222cd18cf55c7e318087ff
Jose Pedro Bernardes <josepedro.bernardes1@gmail.com>
2023-04-30 01:24:14 -0300

    feat: adiciona o controller responsável pela resposta do get users.

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import createUserService from "../services/users/createUser.services";
-import { TUserRequest } from "../interfaces/users.interfaces";
+import { IUserAdmin, TUserRequest } from "../interfaces/users.interfaces";
+import listUsersService from "../services/users/listUsers.services";
 
 const createUser = async (req: Request, res: Response): Promise<Response> => {
   const payload: TUserRequest = req.body;
@@ -9,7 +10,10 @@ const createUser = async (req: Request, res: Response): Promise<Response> => {
 };
 
 const listUsers = async (req: Request, res: Response): Promise<Response> => {
-  return res.status(200).json();
+  const user: IUserAdmin = res.locals.user;
+  const usersList = await listUsersService(user);
+
+  return res.status(200).json(usersList);
 };
 
 const getUser = async (req: Request, res: Response): Promise<Response> => {
